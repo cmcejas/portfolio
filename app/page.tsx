b2106d4cdcf07bd63e1346b9954feded4383b088
Carlos <carlosmcejas@gmail.com>
2023-09-03 23:57:24 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
               <AiFillInstagram href="" className="cursor-pointer hover:rotate-6 hover:text-6xl duration-200 ease-in-out"/>
             </a>
           </div>
-        <div className="flex justify-center fade-in-up text-slate-400 text-4xl pt-36 invisible lg:visible">
+        <div className="flex justify-center fade-in-up text-slate-400 text-4xl pt-36 lg:visible">
           <a href="#two"><BsFillArrowDownCircleFill className="animate-bounce" /></a>
         </div>
         </section>
