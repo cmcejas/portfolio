6b4dd8a857e7bf8b7efde2423e7fdb8b953e141b
Carlos <carlosmcejas@gmail.com>
2023-09-08 22:50:40 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
         </div>
 
           <li className="md:invisible flex justify-center fade-in-up">
-              <a className="bg-slate-700 text-base text-white px-10 py-5 rounded-md hover:transform hover:-translate-y-1 transition-transform duration-300 flex items-center space-x-2" href="Resume.pdf" download>
+              <a className="bg-slate-700 text-lg text-white px-10 py-5 rounded-md hover:transform hover:-translate-y-1 transition-transform duration-300 flex items-center space-x-2" href="Resume.pdf" download>
                 <span>Resume</span>
                 <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
