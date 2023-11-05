406a5695442dab92f3a6951e44bfe2f2fbe70d1a
cmcool <75821231+cmcool@users.noreply.github.com>
2023-11-05 10:17:06 -0500

    better format

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -39,7 +39,7 @@ const Contact = () => {
 
         <form className="md:text-left text-center" onSubmit={handleSubmit(onSubmit)}>
 
-        <p className="text-left">Email Address</p>
+        <p className="lg:text-left">Email Address</p>
         <input placeholder="Enter your email here" className="placeholder:italic placeholder:text-slate-400 rounded-md border-0 md:px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-white-300 sm:text-sm sm:leading-6 text-black"
             {...register("email")}
             type="email"
@@ -47,7 +47,7 @@ const Contact = () => {
 
         <br></br><br></br>
 
-        <p className="text-left">Subject</p>
+        <p className="lg:text-left">Subject</p>
         <input placeholder="Enter your title here" className="placeholder:italic placeholder:text-slate-400 rounded-md border-0 md:px-32 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inherit focus:ring-white-300 sm:text-sm sm:leading-6 text-black"
             {...register("subject")}
             type="subject"
@@ -55,7 +55,7 @@ const Contact = () => {
 
         <br></br><br></br>
 
-        <p className="text-left">Body Text</p>
+        <p className="lg:text-left">Body Text</p>
         <input placeholder="Enter your body text here" className="placeholder:italic placeholder:text-slate-400 rounded-md border-0 md:px-32 py-40 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-white-300 sm:text-sm sm:leading-6 text-black"
             {...register("body")}
             type="body"
