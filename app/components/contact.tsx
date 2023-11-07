efe4381a895a55c073e164ff4345f0c6ed70be60
cmcool <75821231+cmcool@users.noreply.github.com>
2023-11-07 00:10:19 -0500

    added notifications upon submission of contact

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react'
 import axios from "axios";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface FormData {
   email: string;
@@ -20,11 +22,20 @@ const Contact = () => {
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     axios
-      .post("https://eoiwigmaejhiwu7.m.pipedream.net", data)
+      .post("", data)
       .then((response) => {
         setSuccessMessage(
-          `Thanks for the message! Check your inbox for a response soon!`
-        );
+          `Thanks for the message! Check your inbox for a confirmation before submitting another one.`);
+        toast.success("Form submitted successfully!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          }); // Display a success toast
         window.location.href = "#one";
       })
       .catch((e) => console.error(e));
@@ -32,15 +43,14 @@ const Contact = () => {
 
   return (
     <div id="three" className="m-auto w-3/4 md:w-1/4 bg-bgc2 text-white text-lg">
-      
-        <h2 className="flex justify-center items-center text-transparent bg-clip-text cursor-pointer bg-gradient-to-tl from-teal-200 to-teal-500 px-4 py-2 rounded-md hover:transform hover:-translate-y-1 transition-transform duration-300 md:py-2 font-black md:font-bold font-poppins text-4xl lg:text-6xl">
-          <a href="#three">Contact Me</a>
-        </h2>
+      <ToastContainer />
+      <h2 className="flex justify-center items-center text-transparent bg-clip-text cursor-pointer bg-gradient-to-tl from-teal-200 to-teal-500 px-4 py-2 rounded-md hover:transform hover:-translate-y-1 transition-transform duration-300 md:py-2 font-black md:font-bold font-poppins text-4xl lg:text-6xl">
+        <a href="#three">Contact Me</a>
+      </h2>
 
-        <br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br>
 
-        <form className="md:text-left text-center flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-
+      <form className="md:text-left text-center flex flex-col" onSubmit={handleSubmit(onSubmit)}>
         <p className="text-left">Email Address</p>
         <textarea placeholder="Enter your email here" className="placeholder:italic placeholder:text-slate-400 rounded-md border-0 px-2 py-1 md:w-[250px] h-[32px] text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-white-300 sm:text-sm sm:leading-6 text-black"
             {...register("email")}
@@ -67,10 +77,10 @@ const Contact = () => {
         </button>
         <br></br><br></br>
         {successMessage && <p>{successMessage}</p>}
-        </form>
+      </form>
 
-        <br></br>
-        <br></br>
+      <br></br>
+      <br></br>
 
     </div>
   );
---
+++ app/components/success.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Success(){
+  const notify = () => toast("Wow so easy!");
+
+  return (
+    <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Success;
