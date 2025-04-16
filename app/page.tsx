32dfd77833c7e456cd73a398ba204ef557ceb5e3
Carlos <carlosmcejas@gmail.com>
2025-04-16 12:18:12 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
               <AiFillLinkedin href="" className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"/>
             </a>
             <a target="_blank" href="https://x.com/cmcejas">
-              <FaSquareXTwitter href="" className="text-[42px] duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"/>
+              <FaSquareXTwitter href="" className="text-[42px] duration-200 ease-in-out cursor-pointer pt-1 hover:rotate-6 hover:text-6xl"/>
             </a>
             <a target="_blank" href="https://www.instagram.com/cmcejas_/">
               <AiFillInstagram href="" className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"/>
