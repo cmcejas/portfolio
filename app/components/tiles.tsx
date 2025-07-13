08f187102198edcce1c54763b7f0421619c86b68
cmcejas <carlosmcejas@gmail.com>
2025-07-12 22:35:27 -0400

    update to college stuff

--- app/components/tiles.tsx
+++ app/components/tiles.tsx
@@ -1,128 +1,176 @@
-import Image from 'next/image';
-import code from 'public/code.png';
-import education from 'public/education.png'
-import os from 'public/os.png';
-import briefcase from 'public/briefcase.png';
-import languages from 'public/languages.png';
-import guitar from 'public/guitar.png';
+import Image from "next/image";
+import code from "public/code.png";
+import education from "public/education.png";
+import os from "public/os.png";
+import briefcase from "public/briefcase.png";
+import languages from "public/languages.png";
+import guitar from "public/guitar.png";
 
 const Tiles = () => {
-    return(
+  return (
+    <section id="two" className="pt-30 md:pt-0 lg:pt-0">
+      <br className="invisible md:visible"></br>
+      <div>
+        <h3 className="py-1 text-5xl font-bold text-white">About Me</h3>
+      </div>
 
-        <section id="two" className="pt-30 md:pt-0 lg:pt-0">
-        <br className="invisible md:visible"></br>
-        <div>
-          <h3 className="py-1 text-5xl font-bold text-white">About Me</h3>
+      <div className="gap-10 px-7 md:px-10 lg:flex-wrap lg:flex">
+        <div
+          className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300"
+          style={{ boxShadow: "0 0 20px rgba(200, 200, 200, 0.2)" }}
+        >
+          <Image
+            src={education}
+            width={250}
+            height={250}
+            alt=""
+            className="pb-3 mx-auto"
+          />
+          <h3 className="pt-8 pb-2 text-lg font-medium text-white">
+            Education
+          </h3>
+          <p className="py-2 text-slate-300">College Student</p>
+          <h4 className="hover:cursor-pointer underline py-3 text-teal-500">
+            <a href="https://www.tcd.ie/" target="_blank">
+              Trinity College Dublin
+            </a>
+          </h4>
+
+          <p className="py-1 text-gray-400">On the 5 year masters program</p>
         </div>
 
-        <div className="gap-10 px-7 md:px-10 lg:flex-wrap lg:flex">
+        <div
+          className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300"
+          style={{ boxShadow: "0 0 20px rgba(200, 200, 200, 0.2)" }}
+        >
+          <Image
+            src={code}
+            width={150}
+            height={150}
+            alt=""
+            className="py-10 mx-auto"
+          />
+          <h3 className="pb-2 text-lg font-medium text-white pt-9">
+            Programming Languages
+          </h3>
+          <h4 className="py-3 text-teal-500">I am fluent in these.</h4>
 
-          <div className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300" style={{ boxShadow: '0 0 20px rgba(200, 200, 200, 0.2)' }}>
-          <Image src={education} width={250} height={250} alt="" className="pb-3 mx-auto"/>
-            <h3 className="pt-8 pb-2 text-lg font-medium text-white">
-              Education
-            </h3>
-            <p className="py-2 text-slate-300">
-              High School Student
-            </p>
-            <h4 className="py-3 text-teal-500">Relevant Courses</h4>
-            
-            <p className="py-1 text-gray-400">Programming I (Python)</p>
-            <p className="py-1 text-gray-400">AP Computer Science A (Java)</p>
-            <p className="py-1 text-gray-400">AP Physics Electricity and Magnetism</p>
-            <p className="py-1 text-gray-400">DE Multi Variable Calculus</p>
-            <p className="py-1 text-gray-400">AP Calc AB/BC</p>
-            <p className="py-1 text-gray-400">Trig/Pre-Calc Honors</p>
-            <p className="py-1 text-gray-400">Algebra II Honors</p>
-            <p className="py-1 text-gray-400">Engineering Design IS</p>
-
-
-          </div>
-
-          <div className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300" style={{ boxShadow: '0 0 20px rgba(200, 200, 200, 0.2)' }}>
-          <Image src={code} width={150} height={150} alt="" className="py-10 mx-auto"/>
-            <h3 className="pb-2 text-lg font-medium text-white pt-9">
-              Programming Languages
-            </h3>
-            <h4 className="py-3 text-teal-500">I am fluent in these.</h4>
-            
-            <p className="py-1 text-gray-400">Python</p>
-            <p className="py-1 text-gray-400">Java</p>
-            <p className="py-1 text-gray-400">HTML</p>
-            <p className="py-1 text-gray-400">CSS</p>
-            <p className="py-1 text-gray-400">ReactJS</p>
-            <p className="py-1 text-gray-400">TailwindCSS</p>
-            <p className="py-1 text-gray-400">NextJS</p>
-
-
-          </div>
-
-          <div className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300" style={{ boxShadow: '0 0 20px rgba(200, 200, 200, 0.2)' }}>
-          <Image src={os} width={218} height={250} alt="" className="pt-3 mx-auto"/>
-            <h3 className="pt-8 pb-2 text-lg font-medium text-white">
-              Operating Systems
-            </h3>
-
-            <h4 className="py-2 text-teal-500">In order from most to least used.</h4>
-            
-            <p className="py-1 text-gray-400">Microsoft Windows</p>
-            <p className="py-1 text-gray-400">Ubuntu based distributions</p>
-            <p className="py-1 text-gray-400">Arch based distributions</p>
-            <p className="py-1 text-gray-400">Debian based distributions</p>
-            <p className="py-1 text-gray-400">Apple Mac OS</p>
-
-
-          </div>
-
-          <div className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300" style={{ boxShadow: '0 0 20px rgba(200, 200, 200, 0.2)' }}>
-          <Image src={briefcase} width={160} height={250} alt="" className="pt-8 mx-auto"/>
-            <h3 className="pt-16 pb-2 text-lg font-medium text-white">
-              Professional Experience
-            </h3>
-
-            <h4 className="py-2 text-teal-500">More Information on Resume.</h4>
-            
-            <p className="py-1 text-gray-400">
-              <span className="font-extrabold "> Web Developer at Parkt</span>
-              <br>
-              </br>
-              I work alongside a team to develop the <span className="font-bold text-teal-500 underline"><a href="https://parkt.app/" target="_blank">website</a></span>.
-            </p>
-            <p className="pt-4 font-bold text-gray-400">Many projects on github.</p>
-
-
-          </div>
-
-          <div className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300" style={{ boxShadow: '0 0 20px rgba(200, 200, 200, 0.2)' }}>
-          <Image src={languages} width={210} height={250} alt="" className="pb-3 mx-auto"/>
-            <h3 className="pt-8 pb-2 text-lg font-medium text-white">
-              Languages Spoken
-            </h3>
-            <p className="py-1 text-gray-400">English Bilingual Proficiency</p>
-            <p className="py-1 text-gray-400">Spanish Bilingual Proficiency</p>
-            <p className="py-1 text-gray-400">French Limited Working Proficiency</p>
-
-            <h4 className="pt-3 text-teal-500">Currently Learning</h4>
-            
-            <p className="py-1 text-gray-400">German Elementary Level</p>
-
-          </div>
-
-          <div className="w-[100%] 3ti:w-[30%] text-center p-6 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300" style={{ boxShadow: '0 0 20px rgba(200, 200, 200, 0.2)' }}>
-          <Image src={guitar} width={270} height={250} alt="" className="pb-1 mx-auto"/>
-            <h3 className="pb-2 text-lg font-medium text-white">
-              Hobbies
-            </h3>
-            <p className="py-1 text-gray-400">Club and High School Soccer</p>
-            <p className="py-1 text-gray-400">Acoustic and Electric Guitar</p>
-            <p className="py-1 text-gray-400">Programming personal projects</p>
-            <p className="py-1 text-gray-400">School Clubs <span className="font-bold text-teal-500 cursor-pointer"><a href="#one">listed on resume </a></span></p>
-          </div>
-
+          <p className="py-1 text-gray-400">Python</p>
+          <p className="py-1 text-gray-400">Java</p>
+          <p className="py-1 text-gray-400">HTML</p>
+          <p className="py-1 text-gray-400">CSS</p>
+          <p className="py-1 text-gray-400">ReactJS</p>
+          <p className="py-1 text-gray-400">TailwindCSS</p>
+          <p className="py-1 text-gray-400">NextJS</p>
         </div>
-      </section>
 
-    );
+        <div
+          className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300"
+          style={{ boxShadow: "0 0 20px rgba(200, 200, 200, 0.2)" }}
+        >
+          <Image
+            src={os}
+            width={218}
+            height={250}
+            alt=""
+            className="pt-3 mx-auto"
+          />
+          <h3 className="pt-8 pb-2 text-lg font-medium text-white">
+            Operating Systems
+          </h3>
 
+          <h4 className="py-2 text-teal-500">
+            In order from most to least used.
+          </h4>
+
+          <p className="py-1 text-gray-400">Microsoft Windows</p>
+          <p className="py-1 text-gray-400">Ubuntu based distributions</p>
+          <p className="py-1 text-gray-400">Arch based distributions</p>
+          <p className="py-1 text-gray-400">Debian based distributions</p>
+          <p className="py-1 text-gray-400">Apple Mac OS</p>
+        </div>
+
+        <div
+          className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300"
+          style={{ boxShadow: "0 0 20px rgba(200, 200, 200, 0.2)" }}
+        >
+          <Image
+            src={briefcase}
+            width={160}
+            height={250}
+            alt=""
+            className="pt-8 mx-auto"
+          />
+          <h3 className="pt-16 pb-2 text-lg font-medium text-white">
+            Professional Experience
+          </h3>
+
+          <h4 className="py-2 text-teal-500">More Information on Resume.</h4>
+
+          <p className="py-1 text-gray-400">
+            <span className="font-extrabold "> Web Developer at Parkt</span>
+            <br></br>I work alongside a team to develop the{" "}
+            <span className="font-bold text-teal-500 underline">
+              <a href="https://parkt.app/" target="_blank">
+                website
+              </a>
+            </span>
+            .
+          </p>
+          <p className="pt-4 font-bold text-gray-400">
+            Many projects on github.
+          </p>
+        </div>
+
+        <div
+          className="w-[100%] 3ti:w-[30%] text-center p-10 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300"
+          style={{ boxShadow: "0 0 20px rgba(200, 200, 200, 0.2)" }}
+        >
+          <Image
+            src={languages}
+            width={210}
+            height={250}
+            alt=""
+            className="pb-3 mx-auto"
+          />
+          <h3 className="pt-8 pb-2 text-lg font-medium text-white">
+            Languages Spoken
+          </h3>
+          <p className="py-1 text-gray-400">English Bilingual Proficiency</p>
+          <p className="py-1 text-gray-400">Spanish Bilingual Proficiency</p>
+          <p className="py-1 text-gray-400">
+            French Limited Working Proficiency
+          </p>
+
+          <h4 className="pt-3 text-teal-500">Currently Learning</h4>
+
+          <p className="py-1 text-gray-400">German Elementary Level</p>
+        </div>
+
+        <div
+          className="w-[100%] 3ti:w-[30%] text-center p-6 rounded-xl my-10 hover:transform hover:-translate-y-2 transition-transform duration-300"
+          style={{ boxShadow: "0 0 20px rgba(200, 200, 200, 0.2)" }}
+        >
+          <Image
+            src={guitar}
+            width={270}
+            height={250}
+            alt=""
+            className="pb-1 mx-auto"
+          />
+          <h3 className="pb-2 text-lg font-medium text-white">Hobbies</h3>
+          <p className="py-1 text-gray-400">Club and High School Soccer</p>
+          <p className="py-1 text-gray-400">Acoustic and Electric Guitar</p>
+          <p className="py-1 text-gray-400">Programming personal projects</p>
+          <p className="py-1 text-gray-400">
+            School Clubs{" "}
+            <span className="font-bold text-teal-500 cursor-pointer">
+              <a href="#one">listed on resume </a>
+            </span>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Tiles;
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
-'use client'
+"use client";
 
-import Tiles from '../app/components/tiles';
-import Contact from '../app/components/contact';
+import Tiles from "../app/components/tiles";
+import Contact from "../app/components/contact";
 import {
-  AiFillTwitterCircle, 
-  AiFillLinkedin, 
-  AiFillGithub, 
-  AiFillInstagram
-} from 'react-icons/ai';
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import {BsFillArrowDownCircleFill} from 'react-icons/bs'
-import {FiExternalLink} from 'react-icons/fi'
-import Image from 'next/image';
-import code from '../public/code.png';
-import education from '../public/education.png'
-import os from '../public/os.png';
-import briefcase from '../public/briefcase.png';
-import languages from '../public/languages.png';
-import guitar from '../public/guitar.png';
-import { link } from 'fs';
- 
-import { useState } from 'react'
-import Typewriter from 'typewriter-effect';
+import { BsFillArrowDownCircleFill } from "react-icons/bs";
+import { FiExternalLink } from "react-icons/fi";
+import Image from "next/image";
+import code from "../public/code.png";
+import education from "../public/education.png";
+import os from "../public/os.png";
+import briefcase from "../public/briefcase.png";
+import languages from "../public/languages.png";
+import guitar from "../public/guitar.png";
+import { link } from "fs";
+
+import { useState } from "react";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
@@ -29,65 +29,98 @@ export default function Home() {
       <main className="px-2 bg-gradient-to-b from-bgc1 to-bgc2 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="flex justify-between invisible md:visible md:py-10 md:mb-8 lg:mb-10">
-            <h1 className="text-xl text-white fade-in-up">developed by cmcejas</h1>
+            <h1 className="text-xl text-white fade-in-up">
+              developed by cmcejas
+            </h1>
 
             <ul className="flex items-center">
-
-            <li className="fade-in-up">
-              <a className="flex items-center px-4 py-2 ml-8 space-x-2 text-white transition-transform duration-300 rounded-md bg-slate-700 hover:transform hover:-translate-y-1" href="Resume.pdf" target="_blank">
-                <span>My Resume</span>
-                <FiExternalLink className="text-2xl"/>
-              </a>
-            </li>
-
+              <li className="fade-in-up">
+                <a
+                  className="flex items-center px-4 py-2 ml-8 space-x-2 text-white transition-transform duration-300 rounded-md bg-slate-700 hover:transform hover:-translate-y-1"
+                  href="Resume.pdf"
+                  target="_blank"
+                >
+                  <span>My Resume</span>
+                  <FiExternalLink className="text-2xl" />
+                </a>
+              </li>
             </ul>
           </nav>
           <div className="px-6 md:p-6">
-            <h2 className="md:text-center fade-in-up text-[42px] md:py-2 bg-clip-text text-transparent text-white font-extrabold md:font-medium font-poppins md:text-6xl">Carlos Manuel <span className="font-bold">Cejas</span></h2>
+            <h2 className="md:text-center fade-in-up text-[42px] md:py-2 bg-clip-text text-transparent text-white font-extrabold md:font-medium font-poppins md:text-6xl">
+              Carlos Manuel <span className="font-bold">Cejas</span>
+            </h2>
             <br></br>
-              <h3 className="text-2xl md:text-center fade-in-up md:py-2 md:text-3xl text-slate-300">
-                <div className="text-teal-300">
-                  <Typewriter
-                    options={{
-                      strings: ['Senior High School Student', 'Bot Programmer', 'Web-App Developer', 'DECA State Qualifier', 'NHS Member', 'Youth and Government Treasurer', 'Jazz Band Guitarist', 'Club and HS Soccer Player'],
-                      autoStart: true,
-                      loop: true,
-                    }}/>
-                  </div>
-                Computer Science Focus
-              </h3>
-            <p className="max-w-2xl py-5 mx-auto text-lg leading-8 text-gray-400 md:text-center fade-in-up md:text-xl md:pt-8">
-            I am a high school student focused on computer science. 
-            My interest began at a young age as I started to program in Python since I was 8 years old. 
-            I am always learning more in the computer science field and am excited to create new groundbreaking projects. 
+            <h3 className="text-2xl md:text-center fade-in-up md:py-2 md:text-3xl text-slate-300">
+              <div className="text-teal-300">
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Computer Science Student",
+                      "Bot Programmer",
+                      "Web-App Developer",
+                      "DECA State Qualifier",
+                      "NHS Member",
+                      "Youth and Government Treasurer",
+                      "Jazz Band Guitarist",
+                      "Soccer Player and Enthusiast",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </div>
+              Attending Trinity College Dublin
+            </h3>
+            <p className="text-balance max-w-2xl py-5 mx-auto text-lg leading-8 text-gray-400 md:text-center fade-in-up md:text-xl md:pt-4">
+              I am a college student pursuing computer science, a field I’ve
+              been passionate about since childhood. I began programming in
+              Python at the age of 8 and have been building on that foundation
+              ever since. I’m always eager to learn more and excited to develop
+              innovative, impactful projects.
             </p>
           </div>
           <div className="flex justify-center gap-12 pt-3 text-5xl fade-in-up md:gap-16 text-slate-700">
             <a target="_blank" href="https://github.com/cmcejas">
-              <AiFillGithub className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"/>
+              <AiFillGithub className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl" />
             </a>
             <a target="_blank" href="https://www.linkedin.com/in/cmcejas/">
-              <AiFillLinkedin href="" className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"/>
+              <AiFillLinkedin
+                href=""
+                className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"
+              />
             </a>
             <a target="_blank" href="https://x.com/cmcejas">
-              <FaSquareXTwitter href="" className="text-[48px] duration-200 ease-in-out cursor-pointer pt-1 hover:rotate-6 hover:text-6xl"/>
+              <FaSquareXTwitter
+                href=""
+                className="text-[47px] duration-200 ease-in-out cursor-pointer pt-1 hover:rotate-6 hover:text-6xl"
+              />
             </a>
             <a target="_blank" href="https://www.instagram.com/cmcejas_/">
-              <AiFillInstagram href="" className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"/>
+              <AiFillInstagram
+                href=""
+                className="duration-200 ease-in-out cursor-pointer hover:rotate-6 hover:text-6xl"
+              />
             </a>
           </div>
 
           <li className="flex justify-center mt-20 md:mt-0 md:hidden fade-in-up">
-            <a className="flex items-center py-3 space-x-2 text-xl text-white transition-transform duration-300 rounded-lg bg-slate-700 px-7 hover:transform hover:-translate-y-1" style={{ boxShadow: '0 0 23px rgba(200, 200, 200, 0.2)' }} href="Resume.pdf" target="_blank">
+            <a
+              className="flex items-center py-3 space-x-2 text-xl text-white transition-transform duration-300 rounded-lg bg-slate-700 px-7 hover:transform hover:-translate-y-1"
+              style={{ boxShadow: "0 0 23px rgba(200, 200, 200, 0.2)" }}
+              href="Resume.pdf"
+              target="_blank"
+            >
               <span>My Resume</span>
-              <FiExternalLink className="text-2xl"/>
+              <FiExternalLink className="text-2xl" />
             </a>
           </li>
-          
-        <div className="flex justify-center invisible text-4xl fade-in-up text-slate-400 pt-20 lg:visible">
-            <a href="#two"><BsFillArrowDownCircleFill className="animate-bounce" /></a>
-        </div>
-          
+
+          <div className="flex justify-center invisible text-4xl fade-in-up text-slate-400 pt-20 lg:visible">
+            <a href="#two">
+              <BsFillArrowDownCircleFill className="animate-bounce" />
+            </a>
+          </div>
         </section>
 
         {/* second page */}
@@ -103,7 +136,6 @@ export default function Home() {
       {/* third page */}
 
       <Contact />
-
     </div>
-  )
+  );
 }
